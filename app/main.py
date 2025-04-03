b29e234a173d93006fd2785f1e85749894f037a2
luis112002 <148033715+luis112002@users.noreply.github.com>
2025-04-03 18:48:33 -0500

    Proyecto

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import random
 from pathlib import Path
 
 app = FastAPI()
 
-# Obtener la ruta absoluta del archivo actual
 BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+# Montar carpeta static para servir archivos CSS
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def dashboard(request: Request):
